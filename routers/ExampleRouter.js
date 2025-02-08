d0966e9b0cd02e98249618f6f4a0c3baa73d422c
Ronkov200 <devil200921@gmail.com>
2025-02-08 23:49:05 +0300

    UPD index.js

--- routers/ExampleRouter.js
+++ routers/ExampleRouter.js
@@ -1,7 +1,14 @@
 import ExampleController from '../controllers/ExampleController.js'
 
+import { Router } from 'express';
+
+
+const router = new Router();
+
 
 router.post('/login', ExampleController.login);
 router.post('/logout', ExampleController.logout);
 router.post('/signup', ExampleController.signup);
 router.post('/refresh', ExampleController.refresh);
+
+export default router;
